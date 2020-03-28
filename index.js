df4b897553f4e50e255d43549cace815a6aea91d
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-03-28 20:43:47 +0530

    Update index.js
    Now fix bug

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const fetch = require("node-fetch"),
 	url = "https://bot-listweb.glitch.me/api/stats/";
-async function apiPost(client, token) {
-	if (!token) return console.log("Invalid token provided!");
-	if (!client) return console.log("Make sure you provided your bot's instance");
+async function apiPost(id, client, token) {
+	if (!token) return console.log("[ BWL ] Invalid token provided!");
+	if (!client) return console.log("[ BWL ] Make sure you provided your bot's instance");
 	const headers = {
 		"Content-Type": "application/json",
 		authorization: `${token}`
 	};
-	if (client.guilds.cache !== undefined) {
+	if (client.guilds.cache.size !== undefined||null) {
 		const body = {
 			serverCount: client.guilds.cache.size
 		};
-		const response = await fetch(`${url}/${client}`, {
+		const response = await fetch(`${url}/${id}`, {
 			menthod: "POST",
 			headers: headers,
 			body: JSON.stringify(body)
 		});
 		const jsonResponse = response.json();
-		if (jsonResponse.success == true) return console.log("Server count posted!");
-		else return console.log("Error while posting server count");
-	} else if (client.guilds.cache == undefined) {
+		if (jsonResponse.success === "true") return console.log("[ BWL ] Server count posted!");
+		else return console.log("[ BWL ] Error while posting server count");
+	} else if (client.guilds.cache.size === undefined||null) {
 		const body = {
 			serverCount: client.guilds.size
 		};
-		const response = await fetch(`${url}/${client}`, {
+		const response = await fetch(`${url}/${id}`, {
 			menthod: "POST",
 			headers: headers,
 			body: JSON.stringify(body)
 		});
 		const jsonResponse = response.json();
-		if (jsonResponse.success == true) return console.log("Server count posted!");
-		else return console.log("Error while posting server count");
-	} else return console.log("Make sure you provided your bot's instance");
+		if (jsonResponse.success == true) return console.log("[ BWL ] Server count posted!");
+		else return console.log("[ BWL ] Error while posting server count");
+	} else return console.log("[ BWL ] Make sure you provided your bot's instance");
 }
 module.exports = apiPost;
