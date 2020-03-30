ede6067253e1c4253bc506936ff6a13dcb11c4e5
SoulHarsh007 <harshtheking@hotmail.com>
2020-03-30 17:50:14 +0530

    Rebuild code to support Discord.js v12+ and v11.x

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # botlist-web-api
+
+official module to interact with bot-listweb api
+
+## Installation
+
+`npm i bot-listweb-api`
+
+## Example
+
+### Example of posting server count with Discord.js
+
+```js
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const BW = require("dblapi.js");
+const botlistApi = new BW("Your token", client);
+```
+
+### Example of posting server count manually with Discord.js
+
+```js
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const BW = require("dblapi.js");
+const botlistApi = new BW("Your token", client);
+botlistApi.apiPost();
+```
--- index.js
+++ index.js
@@ -1,36 +1,49 @@
 const fetch = require("node-fetch"),
-	url = "https://bot-listweb.glitch.me/api/stats/";
-async function apiPost(client, token) {
-	if (!token) return console.log("Invalid token provided!");
-	if (!client) return console.log("Make sure you provided your bot's instance");
-	const headers = {
-		"Content-Type": "application/json",
-		authorization: `${token}`
-	};
-	if (client.guilds.cache !== undefined) {
-		const body = {
-			serverCount: client.guilds.cache.size
-		};
-		const response = await fetch(`${url}/${client}`, {
-			menthod: "POST",
-			headers: headers,
-			body: JSON.stringify(body)
-		});
-		const jsonResponse = response.json();
-		if (jsonResponse.success == true) return console.log("Server count posted!");
-		else return console.log("Error while posting server count");
-	} else if (client.guilds.cache == undefined) {
-		const body = {
-			serverCount: client.guilds.size
-		};
-		const response = await fetch(`${url}/${client}`, {
-			menthod: "POST",
-			headers: headers,
-			body: JSON.stringify(body)
-		});
-		const jsonResponse = response.json();
-		if (jsonResponse.success == true) return console.log("Server count posted!");
-		else return console.log("Error while posting server count");
-	} else return console.log("Make sure you provided your bot's instance");
+  url = "https://bot-listweb.glitch.me/api/stats/";
+class botWeblist {
+  constructor(token, client) {
+    this.token = token;
+    this.client = client;
+    this.client.on("ready", () => {
+      this.apiPost();
+    });
+  }
+
+  async apiPost() {
+    if (!this.token) return console.log("Invalid token provided!");
+    if (!this.client)
+      return console.log("Make sure you provided your bot's instance");
+    const headers = {
+      "Content-Type": "application/json",
+      authorization: `${this.token}`
+    };
+    if (this.client.guilds.cache !== undefined) {
+      const body = {
+        server_count: this.client.guilds.cache.size
+      };
+      const response = await fetch(`${url}/${this.client.user.id}`, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(body)
+      });
+      const jsonResponse = await response.json();
+      if (jsonResponse.success == true)
+        return console.log("Server count posted!");
+      else return console.log("Error while posting server count");
+    } else if (this.client.guilds.cache == undefined) {
+      const body = {
+        server_count: this.client.guilds.size
+      };
+      const response = await fetch(`${url}/${this.client.user.id}`, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(body)
+      });
+      const jsonResponse = await response.json();
+      if (jsonResponse.success == true)
+        return console.log("Server count posted!");
+      else return console.log("Error while posting server count");
+    } else return console.log("Make sure you provided your bot's instance");
+  }
 }
-module.exports = apiPost;
+module.exports = botWeblist;
