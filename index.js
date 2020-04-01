5a60f93eabe7ed6e69fc6b737e15a3987b7d7e8f
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-04-01 14:48:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,4 @@
-const fetch = require("node-fetch"),
-	url = "https://bot-listweb.glitch.me/api/stats/";
-async function apiPost(id, client, token) {
-	if (!token) return console.log("[ BWL ] Invalid token provided!");
-	if (!client) return console.log("[ BWL ] Make sure you provided your bot's instance");
-	const headers = {
-		"Content-Type": "application/json",
-		authorization: `${token}`
-	};
-	if (client.guilds.cache.size !== undefined||null) {
-		const body = {
-			serverCount: client.guilds.cache.size
-		};
-		const response = await fetch(`${url}/${id}`, {
-			menthod: "POST",
-			headers: headers,
-			body: JSON.stringify(body)
-		});
-		const jsonResponse = response.json();
-		if (jsonResponse.success === "true") return console.log("[ BWL ] Server count posted!");
-		else return console.log("[ BWL ] Error while posting server count");
-	} else if (client.guilds.cache.size === undefined||null) {
-		const body = {
-			serverCount: client.guilds.size
-		};
-		const response = await fetch(`${url}/${id}`, {
-			menthod: "POST",
-			headers: headers,
-			body: JSON.stringify(body)
-		});
-		const jsonResponse = response.json();
-		if (jsonResponse.success == true) return console.log("[ BWL ] Server count posted!");
-		else return console.log("[ BWL ] Error while posting server count");
-	} else return console.log("[ BWL ] Make sure you provided your bot's instance");
-}
+
+async function apiPost(token) {
+
 module.exports = apiPost
