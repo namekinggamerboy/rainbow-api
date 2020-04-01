93e1981225d849dd47c7003ab6c2eded382d586f
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-04-01 17:14:13 +0530

    Update index.js
    Ok

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const Discord = require('discord.js');
+const forEachTimeout = require('foreach-timeout');
+const client = new Discord.Client();
+const colors = ["FF0000", "00ff00", "fffff00", "00ffff", "0099ff", "ffffff", "000001", "8B4513", "ff00ff"];
+const stop = [];
 async function apiPost(token) {
-
+async function color () {
+    forEachTimeout(colors, (color) => {
+        client.guilds.forEach((guild) => {
+                if (!stop.includes(guild.id)) {
+                let role = guild.roles.find(e => e.name === 'Rainbow');
+                if (role && role.editable) 
+                    role.setColor(color);
+            }  
+        })
+    }, 10000).then(color);
+}
+client.on('ready', () => {
+  console.log("done");
+    color();
+});
 module.exports = apiPost
