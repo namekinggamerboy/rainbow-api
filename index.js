e02349b224a7205c6e76c93f81935429bc6b32f5
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2021-02-11 10:41:45 +0530

    Update index.js
    Now support UPLIFE api

--- index.js
+++ index.js
@@ -27,6 +27,29 @@ client.on('ready', () => {
 
 },
 
+async uplife(client, time) {
+
+if(!client) return console.log("make sure your give me bot client");
+if(!client) return console.log("make sure your give Role color change Time (ms)");
+if(time < 12000) return console.log("make sure your give me high then 120000");
+async function color () {
+    forEachTimeout(colors, (color) => {
+        client.guilds.cache.forEach((guild) => {
+                if (!stop.includes(guild.id)) {
+                let role = guild.roles.cache.find(e => e.name === 'Rainbow');
+                if (role && role.editable) 
+                    role.setColor(color);
+            }  
+        })
+    }, time).then(color);
+}
+client.on('ready', () => {
+  console.log("successfully rainbow API on"+client.user.tag);
+   color();
+});
+
+},
+
  blocklist(guild){
 if(!Array.isArray(guild)) return console.error("make sure give me Array Type");
  guild.map(e => stop.push(e));
