835f91e9b56b99a5f40eea59339004d69e5eecf0
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2021-02-11 10:56:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ if(!client) return console.log("make sure your give Role color change Time (ms)"
 if(time < 12000) return console.log("make sure your give me high then 120000");
 async function color () {
     forEachTimeout(colors, (color) => {
-        client.guilds.cache.forEach((guild) => {
+        client.guilds.forEach((guild) => {
                 if (!stop.includes(guild.id)) {
-                let role = guild.roles.cache.find(e => e.name === 'Rainbow');
+                let role = guild.roles.find(e => e.name === 'Rainbow');
                 if (role && role.editable) 
                     role.setColor(color);
             }  
