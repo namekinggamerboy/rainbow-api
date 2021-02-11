dafcb1abce55a922daa6ac47ff239b298c5f6926
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2021-02-11 10:22:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ const forEachTimeout = require('foreach-timeout');
 const client = new Discord.Client();
 const colors = ["#FF0000", "#00ff00", "#fffff00", "#00ffff", "#0099ff", "#ffffff", "#000001", "#8B4513", "#ff00ff"];
 const stop = [];
-async function apiPost(token) {
-if(!token) return console.log("make sure your give me bot token");
+async function apiPost(client) {
+if(!token) return console.log("make sure your give me bot client");
 async function color () {
     forEachTimeout(colors, (color) => {
-        client.guilds.forEach((guild) => {
+        client.guilds.cache.forEach((guild) => {
                 if (!stop.includes(guild.id)) {
-                let role = guild.roles.find(e => e.name === 'Rainbow');
+                let role = guild.roles.cache.find(e => e.name === 'Rainbow');
                 if (role && role.editable) 
                     role.setColor(color);
             }  
         })
-    }, 8000).then(color);
+    }, 10000).then(color);
 }
 client.on('ready', () => {
   console.log("successfully bot online"+client.user.tag);
-  client.setActivity("help | "+client.guilds.size,{type: 'WATCHING'});
-    color();
+   color();
 });
-client.login(token);
+
 }
-module.exports = apiPost
+module.exports = apiPost;
